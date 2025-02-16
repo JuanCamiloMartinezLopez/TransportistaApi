27fecce990d1da4359ef0e5bdbe44aeb44b9b7d5
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2025-02-15 23:29:02 -0500

    feat: modelos para typeORM

---
+++ src/infrastructure/orm/TransportistaModel.ts
@@ -0,0 +1,31 @@
+// src/infrastructure/orm/TransportistaModel.ts
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { RutaModel } from './RutaModel';
+import { VehiculoModel } from './VehiculoModel';
+
+@Entity('transportista')
+export class TransportistaModel {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  nombre!: string;
+
+  @Column({ length: 20 })
+  telefono!: string;
+
+  @Column({ unique: true })
+  correo!: string;
+
+  @CreateDateColumn({ name: 'fecha_creacion', type: 'timestamp' })
+  fechaCreacion!: Date;
+
+  @Column({ name: 'fecha_modificacion', type: 'timestamp' })
+  fechaModificacion!: Date;
+
+  @OneToMany(() => RutaModel, (ruta) => ruta.transportista)
+  rutas!: RutaModel[];
+
+  @OneToMany(() => VehiculoModel, (vehiculo) => vehiculo.transportista)
+  vehiculos!: VehiculoModel[];
+}
